f9bc24365dd926bff48c076c4dbb6b282923a54b
etsegent <tsegiiitm@gamil.com>
2022-04-11 09:33:20 -0700

    My first commit message

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -12,7 +12,7 @@ int main(void)
 int n;
 
 srand(time(0));
-n = rand() - RAND-MAX / 2;
+n = rand() - RAN_MAX / 2;
 
 
 if (n % 10 > 5)
@@ -30,5 +30,5 @@ else
 printf("Last digit of %i is %i and is less than 6 and not 0\n", n, n % 10);
 }
 
-return(0);
+return (0);
 }
