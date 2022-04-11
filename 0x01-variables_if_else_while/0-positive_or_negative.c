51d0696fa2f9e8ff3f6bab4fc4d14afa617797d9
etsegent <tsegiiitm@gamil.com>
2022-04-11 08:00:30 -0700

    My zero commit message

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -3,9 +3,10 @@
 #include <stdio.h>
 
 /**
- * main - Entry point
+ * main - Prints a random number and states whether
+ * it is positive, negative, or zero.
  *
- * Return: Always 0.  
+ * Return: Always 0.
  */
 int main (void)
 {
