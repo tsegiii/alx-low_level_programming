c1aaa8e09d68f535c2ae667d125ab1654a87a48d
etsegent <tsegiiitm@gamil.com>
2022-04-11 08:39:51 -0700

    My task

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -6,9 +6,9 @@
  * main - Prints a random number and states whether
  *         it is positive, negative, or zero
  *
- * Return: Always 0
+ * Return: Always 0.
  */
-int main (void)
+int main(void)
 {
 int n;
 
