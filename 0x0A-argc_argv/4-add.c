6105f5c9b8ff7e9a54be53a995a89e8134cbb105
tsegiii <tsegiiitm@gmail.com>
2022-04-29 19:29:45 +0300

    4th

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * main - prints result follwed by new line
+ * @argc: Number of args
+ * @argv: argumnets
+ * Return: 1 if there is an error otherwise return 0
+*/
+int main(int argc, char *argv[])
+{
+	int i, j, sum = 0;
+
+	for (i = 1; i < argc; i++)
+	{
+		for (j = 0; argv[i][j] != '\0'; j++)
+		{
+			if (argv[i][j] < '0' || argv[i][j] > '9')
+			{
+				printf("Error\n");
+				return (1);
+			}
+		}
+
+		sum += atoi(argv[i]);
+	}
+
+	printf("%d\n", sum);
+
+	return (0);
+}
