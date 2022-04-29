91b8a8362d2d1cdb0b23ab999e54ebee88917e42
tsegiii <tsegiiitm@gmail.com>
2022-04-29 19:01:24 +0300

    z

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * main - gives the name of the file
+ * @argc: number of arguments passed when excuted
+ * @argv: the arguments passed when excuted
+ * Return: 0 on succusfull compltion
+*/
+int main(__attribute__((__unused__)) int argc, char *argv[])
+{
+printf("%s\n", argv[0]);
+return (0);
+}
